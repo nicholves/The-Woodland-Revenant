f7fd84a287f483b7ea021eba4f59ad634c6c8699
nicholves <nicholves@hotmail.com>
2023-11-05 14:40:55 -0500

    changed path to be in cmake and more dynamic

--- src/path_config.h
+++ src/path_config.h
@@ -1,6 +1,1 @@
-#define MATERIAL_DIRECTORY "C:\\Users\\nicho\\Desktop\\School-Work\\Comp-3501\\The-Woodland-Revenant\\resources"
-#define MATERIAL_DIRECTORY "C:\\Users\\hikel\\Desktop\\University\\Fall 2023\\COMP 3501A\\Project\\The-Woodland-Revenant-main\\resources"
-#define MATERIAL_DIRECTORY "C:\\Users\\kevin\\source\\repos\\The-Woodland-Revenant\\resources"
-#define MATERIAL_DIRECTORY "C:\\Users\\benja\\Documents\\School\\3501\\The-Woodland-Revenant\\resources"
-// change to specify your own location here
-
+#define MATERIAL_DIRECTORY CMAKE_PROJ_DIRECTORY "/resources"
